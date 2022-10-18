194528b2f80919f783066ebb0c9e3f8e96d7f456
Frysed <frysed@gmail.com>
2022-10-18 19:48:31 +0300

    add PersonsListVC add PersonInfoVC add basic logic

---
+++ ContactsList/ViewControllers/PersonInfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PersonInfoViewController.swift
+//  ContactsList
+//
+//  Created by Alexander Grishin on 18.10.2022.
+//
+
+import UIKit
+
+class PersonInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
--- ContactsList/ViewControllers/PersonsListViewController.swift
+++ ContactsList/ViewControllers/PersonsListViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class PersonsListViewController: UITableViewController {
 
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var surnameLabel: UILabel!
+    
     var personList = Person.getPerson()
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
+    }
+    
+    //MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        personList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "person", for: indexPath)
+        let person = personList[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = person.name
+        content.secondaryText = person.surname
+        
+        cell.contentConfiguration = content
+        return cell
     }
 
 }
